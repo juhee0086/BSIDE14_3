30af26aadb42db305921090f890cd401146f70bf
Kang juhee <juhee0086@gmail.com>
2023-03-04 02:31:02 +0900

    commit test.

---
+++ src/main/java/com/bside/BSIDE14_3/Bside143Application.java
@@ -0,0 +1,15 @@
+package com.bside.BSIDE14_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bside143Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bside143Application.class, args);
+			System.out.println("test");
+	}
+
+
+}
