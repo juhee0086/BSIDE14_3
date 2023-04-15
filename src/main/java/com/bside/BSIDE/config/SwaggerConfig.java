afbfd3d316732a0dafdc60ad2c789680f1715ee5
Hoon-Hub <gwi01304@naver.com>
2023-04-15 15:58:04 +0900

    기본세팅

--- src/main/java/com/bside/BSIDE/config/SwaggerConfig.java
+++ src/main/java/com/bside/BSIDE/config/SwaggerConfig.java
@@ -3,22 +3,23 @@ package com.bside.BSIDE.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+	@Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("데모 프로젝트 API Document")
+                .version("v0.0.1")
+                .description("데모 프로젝트의 API 명세서입니다.");
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
     }
+	
 }
